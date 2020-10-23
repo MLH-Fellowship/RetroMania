6370db8207ecc618d9582e30ca79832d74327fd1
msaaddev <mrsaadirfan@gmail.com>
2020-10-24 01:21:38 +0500

    📦 NEW: enemy dying

--- src/Enemies/Enemy.gd
+++ src/Enemies/Enemy.gd
@@ -1,9 +1,22 @@
 extends "res://src/Actor.gd"
 
+# moving the enemy to the left, also deactivate physics process at the start of the game
 func _ready():
+	set_physics_process(false)
 	_velocity.x = -speed.x
-	
+
+# when the player hits the enemy to kill
+func _on_StompDetecter_body_entered(body):
+	if body.global_position.y > get_node("StompDetecter").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+# making the enemy move right when it hits something
 func _physics_process(delta):
+	_velocity.y = gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
--- src/Player.gd
+++ src/Player.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends "res://src/Actor.gd"
 
 #Physics values (pixels per second)
 const MOVE_SPEED = 500
@@ -22,7 +22,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
 		move_dir -=1
 	move_and_slide(Vector2(move_dir* MOVE_SPEED, y_velo),Vector2(0, -1))
-		
+
 	#check if player is on ground
 	var grounded = is_on_floor()
 	y_velo += GRAVITY
@@ -33,7 +33,7 @@ func _physics_process(delta):
 		y_velo = 5
 	if y_velo > MAX_FALL_SPEED:
 		y_velo = MAX_FALL_SPEED
-	
+
 	#animations that will run based on input
 	if grounded:
 		if move_dir == 0:
@@ -42,5 +42,3 @@ func _physics_process(delta):
 			$AnimatedSprite.play("run")
 	else:
 		$AnimatedSprite.play("jump")
-		
-
