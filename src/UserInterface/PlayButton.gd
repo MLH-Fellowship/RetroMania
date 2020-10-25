0a73354c32d5f88ce8bedb3d8c169f946e02da58
nkosi-tauro <nkosilati23@yahoo.com>
2020-10-26 01:42:09 +0200

    📦 NEW : Start Menu

---
+++ src/UserInterface/PlayButton.gd
@@ -0,0 +1,15 @@
+#allows code to be run in editor
+tool
+extends Button
+
+
+#get scene dynamically
+export(String, FILE) var next_scene_path = ""
+
+#on button press scene will change
+func _on_PlayButton_button_up():
+	get_tree().change_scene(next_scene_path)
+
+#Error handling
+func _get_configuration_warning() -> String:
+	return "next_scene_path must be set for button to work" if next_scene_path == "" else ""
